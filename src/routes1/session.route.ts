00dcf810c976598a3a28d4059f69d60a5df84a49
Joaquin Angelino Corona <joacoangelino@gmail.com>
2023-02-11 22:27:22 -0300

    new routes proposal

---
+++ src/routes1/session.route.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import { SessionController } from '../controllers/session.controller'
+
+const router: Router = Router()
+/*
+    LOGIN ROUTES
+*/
+router.get('/logout', SessionController.logout)
+
+router.post('/login', SessionController.login)
+
+export default router
